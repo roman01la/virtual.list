915e05bbd11ebf10c4613cdf132857c2cbf8a4ef
Roman Liutikov <roman01la@romanliutikov.com>
2017-10-09 00:25:19 +0300

    set overflow-scrolling & use local scroll id

--- src/virtual/list.cljs
+++ src/virtual/list.cljs
@@ -2,22 +2,22 @@
   (:require [rum.core :as rum]
             [goog.object :as gobj]))
 
-(def id* (atom nil))
-
 (defn- handle-id [state timeout]
-  (when-let [id @id*]
+  (when-let [id (:id @state)]
     (js/clearTimeout id))
-  (reset! id* (js/setTimeout
-                #(do
-                   (reset! id* nil)
-                   (swap! state assoc :scrolling? false))
-                timeout)))
+  (swap! state assoc :id
+         (js/setTimeout
+           #(do
+              (swap! state assoc :id nil)
+              (swap! state assoc :scrolling? false))
+           timeout)))
 
 (rum/defcs v-list <
   rum/static
   (rum/local
     {:offset     0
-     :scrolling? false}
+     :scrolling? false
+     :id         nil}
     ::state)
   [{state ::state :as st}
    {:keys [rows-count
@@ -47,27 +47,28 @@
                                 :offset (-> st :rum/react-component .-base (gobj/get "scrollTop"))
                                 :scrolling? true))]
 
-       [:div {:on-scroll handle-scroll
-              :style     {:height     height
-                          :overflow-y "auto"}}
-        [:div
-         {:style {:position   "relative"
-                  :overflow   "hidden"
-                  :min-height "100%"
-                  :width      "100%"
-                  :height     (* row-height max-rows-count)}}
-         [:div
-          {:style {:position    "absolute"
-                   :will-change "transform"
-                   :transform   (str "translate3d(0, " (* start row-height) "px, 0)")
-                   :left        0
-                   :height      "100%"
-                   :width       "100%"
-                   :overflow    "visible"}}
-          (loop [idx start
-                 out (array)]
-                (if (< idx end)
-                  (do
-                    (.push out (render-row {:idx idx :scrolling? scrolling?}))
-                    (recur (inc idx) out))
-                  out))]]]))
+    [:div {:on-scroll handle-scroll
+           :style     {:height                     height
+                       :overflow-y                 "auto"
+                       :-webkit-overflow-scrolling "touch"}}
+     [:div
+      {:style {:position   "relative"
+               :overflow   "hidden"
+               :min-height "100%"
+               :width      "100%"
+               :height     (* row-height max-rows-count)}}
+      [:div
+       {:style {:position    "absolute"
+                :will-change "transform"
+                :transform   (str "translate3d(0, " (* start row-height) "px, 0)")
+                :left        0
+                :height      "100%"
+                :width       "100%"
+                :overflow    "visible"}}
+       (loop [idx start
+              out (array)]
+         (if (< idx end)
+           (do
+             (.push out (render-row {:idx idx :scrolling? scrolling?}))
+             (recur (inc idx) out))
+           out))]]]))
